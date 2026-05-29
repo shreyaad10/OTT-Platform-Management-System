20565ac8390046c0bac401f8211e6e4b5d3c1356
Shreya Desai <shreyaddesai11@gmail.com>
2026-05-29 12:52:49 +0530

    Updated GUI

--- GUI.py
+++ GUI.py
@@ -7,4 +7,7 @@ root.title("My First GUI")
 label1=tk.Label(root, text="OTT Platform Management System", font=('Times New Roman',24))
 label1.pack(padx=20,pady=20)
 
+textbox1= tk.Text(root,height=3,font=('Times New Roman',18))
+textbox1.pack()
+
 root.mainloop()
